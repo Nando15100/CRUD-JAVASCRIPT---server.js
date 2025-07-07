fbcfe74f110d8ccb2acd1f4c736d2da3d51838e8
Nando15100 <fernando.202111208@unilasalle.edu.br>
2025-07-07 20:54:48 -0300

    Create Rota_login.js

---
+++ Rota_login.js
@@ -0,0 +1,18 @@
+// ROTA DE LOGIN DO TIPO DO USUÁRIO
+app.post('/tipo_usuario_Login', async (req, res) => {
+  const { usuario, senha } = req.body;
+
+  try {
+    const Nome_Da_Colecao_Primeira_Letra_MINUSCULA_Referente_Tipo_Usuario = await Nome_Da_Colecao_Primeira_Letra_MAUISCULA_Referente_Tipo_Usuario.findOne({nome: usuario });
+
+    if (!Nome_Da_Colecao_Primeira_Letra_MINUSCULA_Referente_Tipo_Usuario || Nome_Da_Colecao_Primeira_Letra_MINUSCULA_Referente_Tipo_Usuario.senha !== senha) {
+      return res.status(401).json({ message: 'Usuário ou senha inválidos!' });
+    }
+
+    res.status(200).json({ autenticado: true });
+
+  } catch (error) {
+    console.error('Erro ao realizar login:', error);
+    res.status(500).json({ message: 'Erro interno no servidor' });
+  }
+});
